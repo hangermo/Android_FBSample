458f9f22921f0add1d2bedc86ae8e5dc5c4ecd07
hangermo <hangermo@hangermodeair>
2014-08-02 10:55:21 +0800

    Add "ask for permissions"
    Signed-off-by: hangermo <hangermo@hangermodeair>

--- src/hello/sample/testing/NotLoginFragment.java
+++ src/hello/sample/testing/NotLoginFragment.java
@@ -1,5 +1,9 @@
 package hello.sample.testing;
 
+import java.util.Arrays;
+
+import com.facebook.widget.LoginButton;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,11 +11,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class NotLoginFragment extends Fragment {
+	private LoginButton login_button;
 	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
     		Bundle savedInstanceState) {
     		View view = inflater.inflate(R.layout.notlogin_layout, container, false);
+    		//取得使用者授權同意
+    		login_button = (LoginButton)view.findViewById(R.id.login_button);
+    		login_button.setFragment(this);
+    		login_button.setReadPermissions(Arrays.asList("user_likes", "user_status"));
     		return view;
     }
 }
